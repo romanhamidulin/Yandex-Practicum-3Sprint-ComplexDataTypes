11c9fcc8fd529ec090b7202ddd46fed2d5fca67a
Роман Хамидулин <romanhamidulin@ya.ru>
2025-03-25 11:54:21 +0500

    Initial commit

---
+++ src/TypeСonversion/Resources.java
@@ -0,0 +1,23 @@
+package TypeСonversion;
+
+class Resources {
+    long gold;
+    byte silver;
+    short wood;
+    long health;
+    boolean hasHelper;
+
+    public Resources(
+            long inputGold,
+            byte inputSilver,
+            short inputWood,
+            long inputHealth,
+            boolean inputHasHelper
+    ) {
+        gold = inputGold;
+        silver = inputSilver;
+        wood = inputWood;
+        health = inputHealth;
+        hasHelper = inputHasHelper;
+    }
+}
